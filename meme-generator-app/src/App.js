99d398068783f38ef7945775ef7b7d7b4d8543a1
Greg <gmgordon15@gmail.com>
2022-06-16 13:22:59 -0400

    Created header and main container for application.

---
+++ meme-generator-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Main-Container">
+      <Header/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ meme-generator-app/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Header() {
+    return (
+        <header className="Header-Container">
+            <h2>Meme Generator</h2>
+        </header>
+    )
+}
+
+export default Header
