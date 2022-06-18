444d844f3c33ffee7e04d7a14c70571823090075
Greg <gmgordon15@gmail.com>
2022-06-18 16:48:21 -0400

    Added button functionality.  When you click on button images are grabbed from object in memesData file

--- meme-generator-app/src/Meme.js
+++ meme-generator-app/src/Meme.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import memesData from './memesData'
 
 
 function Meme() {
+
+const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg" 
+})
+const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+
+function getMemeImage() {
+    const memesArray = allMemeImages.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    setMeme(prevMeme => ({
+        ...prevMeme,
+        randomImage: url
+    }))
+    
+}
+
     return (
         <div className="Meme-Container">
            
-               <form className="Input-Container">
+               <div className="Input-Container">
                   
                   <div className="Input-Div">
                   <input type="text" name="top-text" 
@@ -14,9 +35,9 @@ function Meme() {
                   <input type="text" name="bottom-text" 
                   className="Inputs" placeholder="Bottom Text"/>
                  </div>
-                 <button className="Submit-Btn">Generate New Meme</button>
-               </form>
-           
+                 <button onClick={getMemeImage} className="Submit-Btn" >Generate New Meme</button>
+               </div>
+           <img src={meme.randomImage} alt=""/>
         </div>
     )
 }
